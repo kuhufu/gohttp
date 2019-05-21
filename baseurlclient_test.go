2b548c642ded9ee395e08fe80936607b4b88a41e
kuhufu <huboemail@yeah.net>
2019-05-21 20:08:51 +0800

    update

--- baseurlclient_test.go
+++ baseurlclient_test.go
@@ -6,10 +6,11 @@ import (
 	"testing"
 )
 
-var obj = NewBase("https://starmicro.happyelements.cn/v1", http.DefaultClient)
+var bc = NewBase("https://starmicro.happyelements.cn/v1", http.DefaultClient)
+var bc2 = Base("https://starmicro.happyelements.cn/v1")
 
 func TestInstance_Get(t *testing.T) {
-	s, e := obj.Get("/idol/idollist", map[string]string{
+	s, e := bc.Get("/idol/idollist", map[string]string{
 		"name": "jhon",
 		"age":  "1",
 	}).String()
